6c900b180d3d4a745f2c1b8b6c12b888c0c303f3
luorenjie <lrj193927@gmail.com>
2020-12-31 13:23:47 -0500

    add post method for BeerController

--- src/main/java/org/northernjay/msscbrewery/web/controller/BeerController.java
+++ src/main/java/org/northernjay/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package org.northernjay.msscbrewery.web.controller;
 
 import org.northernjay.msscbrewery.services.BeerService;
 import org.northernjay.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,5 +23,17 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to url
+        headers.add("Location", "/api/v1/beer" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
