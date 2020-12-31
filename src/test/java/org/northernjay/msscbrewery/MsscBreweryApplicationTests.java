68cbe9b7e482fd4a9ddc9552b1c0d8285d637a57
luorenjie <lrj193927@gmail.com>
2020-12-31 12:14:21 -0500

    init project

---
+++ src/test/java/org/northernjay/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.northernjay.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
