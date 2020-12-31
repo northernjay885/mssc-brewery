e87ca0d4ed33e65f357240ca9c24289ca0f34850
luorenjie <lrj193927@gmail.com>
2020-12-31 16:47:50 -0500

    implement deleteBeer

--- src/main/java/org/northernjay/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/org/northernjay/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package org.northernjay.msscbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.northernjay.msscbrewery.web.model.BeerDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to update beer
 
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
